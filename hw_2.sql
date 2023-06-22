3b274414fd3e860804d4171c4581176ae1815367
Roman Kozhin <kozhin.rm@gmail.com>
2023-06-22 11:50:52 +0300

    Домашнее задание. Семинар 2

--- hw_2.sql
+++ hw_2.sql
@@ -41,3 +41,37 @@ CASE
 	ELSE 'Fail'
     END;
 
+
+/*
+Создайте таблицу “orders”, заполните ее значениями.
+Покажите “полный” статус заказа, используя оператор CASE
+*/
+
+CREATE TABLE hw_2.order
+(
+  orderid INT AUTO_INCREMENT PRIMARY KEY,
+  employeeid VARCHAR(10) NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  orderstatus VARCHAR(45) NOT NULL
+);
+
+INSERT hw_2.order(employeeid, amount, orderstatus)
+VALUES
+('e03', 15.00,'OPEN'),
+('e01', 25.50,'OPEN'),
+('e05', 100.70,'CLOSED'),
+('e02', 22.18,'OPEN'),
+('e04', 9.50,'CANCELLED'),
+('e04', 99.99,'OPEN');
+
+SELECT orderid, orderstatus,
+CASE
+	WHEN orderstatus = 'OPEN'
+		THEN 'Order is in open state'
+	WHEN orderstatus = 'CLOSED'
+		THEN 'Order is closed'
+	WHEN orderstatus = 'CANCELLED'
+		THEN 'Order is cancelled'
+	ELSE 'Fail'
+END AS order_summary
+FROM hw_2.order;
