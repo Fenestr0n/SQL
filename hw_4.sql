6d5b80aac824a221cb58833b9fc2f121438a180f
Roman Kozhin <kozhin.rm@gmail.com>
2023-06-26 17:02:52 +0300

    Create table auto

---
+++ hw_4.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE hw_4;
+USE hw_4;
+
+-- Создать таблицу автомобили
+CREATE TABLE AUTO (
+    REGNUM VARCHAR(10) PRIMARY KEY,
+    MARK VARCHAR(10),
+    COLOR VARCHAR(15),
+    RELEASEDT DATE,
+    PHONENUM VARCHAR(15)
+);
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES
+(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221'),
+(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334'),
+(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334'),
+(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332'),
+(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336'),
+(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444'),
+(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', NULL),
+(111117,'BMW', 'СИНИЙ', date'2005-01-01', NULL),
+(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
+
