567f49b74052b69429c33de9a263babb6c7a42e8
Roman Kozhin <kozhin.rm@gmail.com>
2023-06-28 17:00:01 +0300

    Домашнее задание. Семинар 6

--- hw_6.sql
+++ hw_6.sql
@@ -1,6 +1,6 @@
 /*
 Создайте функцию, которая принимает кол-во сек и форматирует их в кол-во дней, часов, минут и секунд.
-Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
+Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds'
 */
 
 DROP FUNCTION IF EXISTS parse_time;
@@ -18,8 +18,29 @@ BEGIN
 	SET ss = cast(round(mod(mod(mod(sec/60/60/24,1)*24,1)*60,1)*60) as char(2));
 	SET result = concat(dd, " days ", hh, " hours ", mm, " minutes ", ss, " seconds");
 	RETURN result;
-
 END //
 DELIMITER ;
 
 SELECT parse_time(123456);
+
+
+/*
+Выведите только четные числа от 1 до 10 включительно.
+Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
+*/
+DROP PROCEDURE IF EXISTS even;
+DELIMITER //
+CREATE PROCEDURE even(IN n INT, IN m INT)
+BEGIN
+	DECLARE result VARCHAR(100) DEFAULT '';
+	IF (n % 2 != 0) THEN SET n = n + 1;
+	END IF;
+	WHILE (n <= m) DO
+		SET result = CONCAT(result, ' ', n);
+        SET n = n + 2;
+	END WHILE;
+    SELECT result;
+END //
+DELIMITER ;
+
+CALL even(1, 10);
