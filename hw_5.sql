80a5fe9cbb78c302471217be101dc6ca52c844d1
Roman Kozhin <kozhin.rm@gmail.com>
2023-06-28 14:14:11 +0300

    Added Task #1

--- hw_5.sql
+++ hw_5.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE hw_5;
 USE hw_5;
 
+
+-- ЗАДАНИЕ №1
+
 -- Создать таблицу автомобили
 CREATE TABLE cars (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,4 +20,16 @@ INSERT cars (name, cost) VALUES
 ('Citroen', 21000),
 ('Hummer', 41400),
 ('Volkswagen', 21600);
+
+-- Создайте представление, в которое попадут автомобили стоимостью до $25_000
+CREATE OR REPLACE VIEW car_price AS
+	SELECT * FROM cars WHERE cost < 25000;
+
+-- Изменить в существующем представлении порог для стоимости: пусть цена будет до $30_000 (используя оператор ALTER VIEW) 
+ALTER VIEW car_price AS
+	SELECT * FROM cars WHERE cost < 30000;
+
+-- Создайте представление, в котором будут только автомобили марки 'Skoda' и 'Audi'
+CREATE OR REPLACE VIEW vag_group AS
+	SELECT * FROM cars WHERE name IN ('Skoda', 'Audi');
 
